cf89e7ada5f37d9a2ffa5b63857019445fe24863
Avinash <avinash8142@gmail.com>
2019-04-08 01:11:44 +0530

    2nd comment

--- restful-web-services/src/main/java/com/web/restfulwebservices/controller/HelloController.java
+++ restful-web-services/src/main/java/com/web/restfulwebservices/controller/HelloController.java
@@ -29,6 +29,7 @@ public class HelloController {
 		System.out.println("config "+propertyConfig);
 		System.out.println("ping method changed");
 //		comment added
+//		2nd comment
 		return "ping called in halloController";
 		
 	}
