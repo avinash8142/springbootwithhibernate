b616a64581c3cd0c53ced2f736e1ff1bb2578fa1
Avinash <avinash8142@gmail.com>
2019-04-08 00:44:14 +0530

    new line added

--- restful-web-services/src/main/java/com/web/restfulwebservices/controller/HelloController.java
+++ restful-web-services/src/main/java/com/web/restfulwebservices/controller/HelloController.java
@@ -28,6 +28,7 @@ public class HelloController {
 	{
 		System.out.println("config "+propertyConfig);
 		System.out.println("ping method changed");
+		System.out.println("new line added");
 		return "ping called in halloController";
 		
 	}
